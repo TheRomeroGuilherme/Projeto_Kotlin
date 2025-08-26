733cade58f0cbfc77df3ea156b6eec7df3b5f882
GUILHERME ROMERO DA ROSA <guilherme.rosa@cs.up.edu.br>
2025-08-25 21:15:35 -0300

    criando um botão para interação

--- app/src/main/java/com/example/projetokotlin/MainActivity.kt
+++ app/src/main/java/com/example/projetokotlin/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.example.projetokotlin
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.gestures.snapping.SnapPosition
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.projetokotlin.ui.theme.ProjetoKotlinTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,18 +36,34 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@SuppressLint("RememberReturnType")
 @Composable
 fun Greeting() {
 //    Text(
 //        text = "Hello!",
 //    )
-//    Column {
+//    Column (modifier = Modifier.
+//    fillMaxSize(). // identificar o tamanho da tela
+//    padding(16.dp), // colocar uma borda
+//        verticalArrangement = Arrangement.Center,// alinhar vertical
+//        horizontalAlignment = Alignment.CenterHorizontally //alinha horizontal
+//    )
+//    {
 //        Text("Construindo o primeiro app");
 //        Text("Esse é meu app!!");
 //    }
-    Row {
-        Text("Construindo o primeiro app");
-        Text("Esse é meu app!!");
+//    Row {
+//        Text("Construindo o primeiro app");
+//        Text(" Esse é meu app!!");
+//    }
+
+    var contador = remember { mutableStateOf(0) }
+
+    Column (modifier = Modifier.fillMaxSize().padding(16.dp), ){
+        Text("contador : ${contador.value}")
+        Button(onClick = {contador.value++}){
+            Text("+")
+        }
     }
 }
 
